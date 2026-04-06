c134d87cc5c2df3c72afb3e14d26d43fac18533b
David John Dacanay <dabidyooow@Davids-MacBook-Air.local>
2026-04-06 12:02:41 +0800

    solved unhashable type: dict

--- main.py
+++ main.py
@@ -17,29 +17,25 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/merger", response_class = HTMLResponse)
 def merger_page(request: Request):
-    return templates.TemplateResponse("merger.html", {"request": request})
+    return templates.TemplateResponse("merger.html", {"request": request, "menu": [{"name": "Merger"}]})
 
 
 @app.get("/", response_class = HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "menu": [{"name": "Home"}]})
 
 @app.get("/contact", response_class = HTMLResponse)
 def contact(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request})
-
+    return templates.TemplateResponse("contact.html", {"request": request, "menu": [{"name": "Contact"}]})
 @app.get("/privacy", response_class = HTMLResponse)
 def privacy(request: Request):
-    return templates.TemplateResponse("privacy.html", {"request": request})
+    return templates.TemplateResponse("privacy.html", {"request": request, "menu": [{"name": "Privacy Policy"}]})
 
 @app.get("/terms", response_class = HTMLResponse)
 def terms(request: Request):
-    return templates.TemplateResponse("terms.html", {"request": request})
-
+    return templates.TemplateResponse("terms.html", {"request": request, "menu": [{"name": "Terms of Service"}]})
 @app.get("/about", response_class = HTMLResponse)
 def about(request: Request):
-    return templates.TemplateResponse("about.html", {"request": request})
-
-
+    return templates.TemplateResponse("about.html", {"request": request, "menu": [{"name": "About"}]})
 app.include_router(merger.router)
 
